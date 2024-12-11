1ba0a8b666de09a50bf51572fc612f3e3285f822
Kiana Heidari <kiana_hedari71@yahoo.com>
2024-12-11 06:15:42 -0500

    changes in sidebar

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const SelectInput = ({ label, options, className, ...rest }) => {
   return (
     <div className="mb-1 w-full px-4">
-      <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+      <label className="block text-sm font-medium text-gray-700 ">
         {label}
       </label>
       <div className="relative">
         <select
-          className={`w-full border border-gray-300 dark:border-gray-700 rounded-sm px-8 py-1 text-sm text-gray-700 dark:bg-gray-800 dark:text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 appearance-none hover:border-blue-400 transition-all duration-200 ease-in-out ${className}`}
+          className={`w-full border border-gray-300  rounded-sm px-8 py-1 text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 appearance-none hover:border-blue-400 transition-all duration-200 ease-in-out ${className}`}
           {...rest}
         >
           {options.map((option, index) => (
@@ -20,7 +20,7 @@ const SelectInput = ({ label, options, className, ...rest }) => {
         {/* Custom dropdown arrow */}
         <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
           <svg
-            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            className="w-5 h-5 text-gray-500 "
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
--- src/components/TextInputGroup.jsx
+++ src/components/TextInputGroup.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TextInputGroup = ({ title, children }) => {
   return (
-    <div className="w-full px-4 py-2 bg-white dark:bg-gray-800">
-      <p className="font-bold text-sm mb-1 text-gray-800 dark:text-gray-200">{title}</p>
+    <div className="w-full px-4 py-2 bg-white ">
+      <p className="font-bold text-sm mb-1 text-gray-800 ">{title}</p>
       {children}
     </div>
   );
